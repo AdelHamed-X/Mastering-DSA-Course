07285055b628447af135c4678caac508b4df50f3
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-06-22 18:18:13 +0300

    Adding lesson insert into array

---
+++ arrays/insert_into_array.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int A[20];
+    int size;
+    int length;
+};
+
+
+void display(struct Array arr)
+{
+    int i;
+
+    for (i = 0; i < arr.length; i++)
+    {
+        printf("%d\n", arr.A[i]);
+    }
+}
+
+void append(struct Array *arr, int value)
+{
+    if (arr->length < arr->size)
+        arr->A[arr->length++] = value;
+}
+
+void add(struct Array *arr, int value, int ind)
+{
+    if (arr->size < ind < 0)
+    {
+        printf("Index is out of range!");
+        return;
+    }
+
+    int i;
+
+    for (i = arr->length; i > ind; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
+    arr->length++;
+    arr->A[i] = value;
+}
+
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+
+    // append(&arr, 2);
+
+    add(&arr, 10, 2);
+
+    display(arr);
+
+    return 0;
+}
